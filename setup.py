13dea5c4b1812eb727c8f0c7b8d71ba3ce9b3289
Richard Jao <richard_jao@dlsu.edu.ph>
2021-09-15 15:49:55 +0800

    added package

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import setuptools
+
+setuptools.setup(
+    name='raceplotly_happiness',
+    version='0.0.1',
+    url='https://github.com/mike-huls/toolbox',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    },
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['plotly', 'pandas', 'numpy', 'datetime'],
+)
